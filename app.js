7f60c0960a67cfb647bb316e4b56ddee92749a8b
vishal donda <vishaldonda89@gmail.com>
2024-05-28 14:57:21 +0530

    Authorization App

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables
 
 const app = express();
+
+// Connect to the database
 connectDB();
+
+// Middleware to parse JSON
 app.use(express.json());
 
+// Define routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
